a5dc80f7d8b018abceb50f05d77da9327324b3c3
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-02-15 20:11:50 +0530

    StripeComp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Work from './components/Work'
+import Stripes from './components/Stripes'
 
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 font-["satoshi "] text-white'>
-      <Navbar />
+      <Navbar/>
       <Work/>
+      <Stripes/>
     </div>
   )
 }
