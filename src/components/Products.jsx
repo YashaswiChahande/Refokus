1127f05e85050d2f2caaafa903c208cbd72a74e3
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-02-17 18:26:24 +0530

    AnimPBoxComp

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './Product'
+import { motion } from "framer-motion"
+import Arqitel from "../assets/videos/Arqitel.webm";
+import Cula from "../assets/videos/Cula.mp4";
+import TTR from "../assets/videos/TTR.webm";
+import yahoo from "../assets/videos/yahoo.webm";
+
 
 function Products() {
 
@@ -34,13 +40,65 @@ function Products() {
             case: true,
           },
     ]
+    
+    const [pos, setPos] = useState(0);
+    
+    const mover = (val) => {
+      setPos(val*23);
+    }
 
   return (
     <div className='mt-32 relative '>
         {
-            products.map((val, index)=> <Product key={index} val = {val} /> )
+            products.map((val, index) => <Product 
+            key={index} 
+            val = {val} 
+            mover = {mover} 
+            count={index} /> 
+                        )
         }
-        <div className='absolute top-0 w-full h-full pointer-events-none'></div>
+        <div className='absolute top-0 w-full h-full pointer-events-none'>
+          <motion.div 
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1], duration: .6 }}
+          className='window absolute w-[32rem] h-[23rem] bg-sky-100 left-[44%]  overflow-hidden '>
+            <motion.div 
+            animate={{y: -pos+ `rem`}} 
+            className='w-full h-full bg-sky-100 '
+            transition={{ease: [0.76, 0, 0.24, 1], duration: .5 }}
+            >
+              <video autoPlay muted loop src={Arqitel}></video>
+            </motion.div>
+
+            <motion.div 
+            animate={{y: -pos+ `rem`}} 
+            className='w-full h-full bg-sky-200 '
+            transition={{ease: [0.76, 0, 0.24, 1], duration: .5 }}
+            >
+              <video autoPlay muted loop src={Cula}></video>
+
+            </motion.div>
+
+            <motion.div 
+            animate={{y: -pos+ `rem`}}
+            className='w-full h-full bg-sky-300 '
+            transition={{ease: [0.76, 0, 0.24, 1], duration: .5 }}
+            >
+              <video autoPlay muted loop src={TTR}></video>
+
+            </motion.div>
+
+            <motion.div 
+            animate={{y: -pos+ `rem`}} 
+            className='w-full h-full bg-sky-400 '
+            transition={{ease: [0.76, 0, 0.24, 1], duration: .5 }}
+            >
+              <video autoPlay muted loop src={yahoo}></video>
+
+            </motion.div>
+          </motion.div>
+        </div>
     </div>
   )
 }
